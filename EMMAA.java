f072f4ea65936939c9b3dc1c202b2153c7f66393
OCHEGER <ochegeremmanuel14@gmail.com>
2024-09-20 12:55:13 +0300

    Create EMMAA.java

---
+++ EMMAA.java
@@ -0,0 +1,12 @@
+public class Emma {
+  public static void main(String[] args) {
+     int age =18;
+     if(age<13){
+     System.out.println("you are achild.");
+     }else if(age<18){
+     System.out.println("yuo are ateenager.");
+   }else{
+   System.out.println("you are an adult.");
+   }
+}
+}
